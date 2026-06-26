a50aea5100702eac5baa9af74d3b3068d8e44399
sebastian1936 <sebastian1936@users.noreply.github.com>
2026-06-26 19:19:06 +0800

    chore: 更新项目内所有图标资源
    替换了应用主图标、托盘图标、安卓启动图标等全平台应用图标资源，统一更新视觉样式

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -92,7 +92,7 @@ pub const LINK_HEADLESS_LINUX_SUPPORT: &str =
     "https://github.com/rustdesk/rustdesk/wiki/Headless-Linux-Support";
 
 lazy_static::lazy_static! {
-    pub static ref HELPER_URL: HashMap<&'static str, &'static str> = HashMap::from([
+    pub static ref HELPER_URL: HashMap<&'static str, &'static str> = HashMap::from([nemo
         ("rustdesk docs home", LINK_DOCS_HOME),
         ("rustdesk docs x11-required", LINK_DOCS_X11_REQUIRED),
         ("rustdesk x11 headless", LINK_HEADLESS_LINUX_SUPPORT),
